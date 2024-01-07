9065801f913675ee07b27d31d157a8570c5924ae
bjakym <bohdan.jakym@kyndryl.com>
2024-01-07 10:42:02 +0100

    Updated provider and backend declaration

--- azure_virtual_network/main.tf
+++ azure_virtual_network/main.tf
@@ -1,23 +1,3 @@
-# We first specify the terraform provider. 
-# Terraform will use the provider to ensure that we can work with Microsoft Azure
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Here we need to mention the Azure AD Application Object credentials to allow us to work with 
-# our Azure account
-# Application (client) ID = client_id
-
-provider "azurerm" {
-  features {}
-}
-
 locals {
   resource_group="bj-grp"
   location="North Europe"
