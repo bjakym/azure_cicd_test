097fd0c2bd92eae95503178781ec7513efa30b14
bjakym <bohdan.jakym@kyndryl.com>
2024-01-07 11:03:38 +0100

    Fixed error with resource group reference

--- azure_virtual_network/main.tf
+++ azure_virtual_network/main.tf
@@ -1,17 +1,16 @@
 locals {
-  resource_group="bj-grp"
-  location="North Europe"
+  resource_group_name = "${var.res_group}"
+  location = "${var.location}"
 }
 
-resource "azurerm_resource_group" "app_grp"{
-  name=local.resource_group
-  location=local.location
+data "azurerm_resource_group" "setup" {
+  name = local.resource_group_name
 }
 
 resource "azurerm_virtual_network" "app_network" {
   name                = "app-network"
   location            = local.location
-  resource_group_name = azurerm_resource_group.app_grp.name
+  resource_group_name = data.azurerm_resource_group.setup.name
   address_space       = ["10.0.0.0/16"]
 
   subnet {
---
+++ azure_virtual_network/variables.tf
@@ -0,0 +1,13 @@
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "location" {
+  type    = string
+  default = "North Europe"
+}
+
+variable "res_group" {
+  type    = string
+  default = "bj-grp"
+}
