23e42ecc9d457f535ee1888379899ef3a60ecd6b
bjakym <bohdan.jakym@kyndryl.com>
2024-01-07 11:07:51 +0100

    Added Subnet-B

--- azure_virtual_network/main.tf
+++ azure_virtual_network/main.tf
@@ -16,5 +16,10 @@ resource "azurerm_virtual_network" "app_network" {
   subnet {
     name           = "Subnet-A"
     address_prefix = "10.0.1.0/24"
-  }  
+  }
+
+  subnet {
+    name           = "Subnet-B"
+    address_prefix = "10.0.2.0/24"
+  }
 }
