480677979b341f899b1ed5a4cad2c67412ffe383
bjakym <bohdan.jakym@kyndryl.com>
2024-01-10 19:16:44 +0100

    Testing matrix

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,8 +3,7 @@ files="../azure_virtual_network/main.tf ../azure_virtual_network/run.sh"
 folders=()
 for f in .github/workflows/kstp-nsxt-cicd.yml azure_virtual_network/run.sh azure_virtual_network/terraform.tf backend/terraform.tf; \
 do \
-    echo "${f}" | egrep -qi ".*tf$"
-    if [ $? -eq 0 ]; then
+    if [ -z ${f##*.tf} ]; then
         echo "Adding $(dirname $f) to folders"; \
         folders+=($(dirname $f)); \
     else
