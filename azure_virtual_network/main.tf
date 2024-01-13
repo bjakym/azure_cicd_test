62eb42be7fc08e50d4f622fd7c60f38ba6bed87a
bjakym <bohdan.jakym@kyndryl.com>
2024-01-13 17:16:02 +0100

    app-network renamed

--- azure_virtual_network/main.tf
+++ azure_virtual_network/main.tf
@@ -5,12 +5,12 @@ resource "azurerm_virtual_network" "app_network" {
   address_space       = ["10.0.0.0/16"]
 
   subnet {
-    name           = "Subnet-A1"
+    name           = "Subnet-A11"
     address_prefix = "10.0.1.0/24"
   }
 
   subnet {
-    name           = "Subnet-B1"
+    name           = "Subnet-B11"
     address_prefix = "10.0.2.0/24"
   }
 }
