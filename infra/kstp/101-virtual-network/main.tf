c0fe5865a9934bd58dd8af2925f4230b5fc146fa
bjakym <bohdan.jakym@kyndryl.com>
2024-01-20 11:17:54 +0100

    Random commit 5 for dres-306

--- infra/kstp/101-virtual-network/main.tf
+++ infra/kstp/101-virtual-network/main.tf
@@ -2,5 +2,5 @@ resource "azurerm_virtual_network" "kstp_network" {
   name                = var.network
   location            = var.location
   resource_group_name = var.res_group
-  address_space       = ["10.1.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
