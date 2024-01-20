ac075e880dca4fe974aba094367c5654cba26cb3
bjakym <bohdan.jakym@kyndryl.com>
2024-01-20 18:36:02 +0100

    Random commit 24 for dres-312

---
+++ infra/kstp/101-nw-sec-group/main.tf
@@ -0,0 +1,43 @@
+##################################################################################
+# CONFIGURATION
+##################################################################################
+
+# App Network Security Group
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = var.location
+  resource_group_name = var.res_group
+
+  # We are creating a rule to allow traffic on port 80
+  security_rule {
+    name                       = "Allow_HTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# DB Network Security Group
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "db-nsg"
+  location            = var.location
+  resource_group_name = var.res_group
+
+  # We are creating a rule to allow traffic on port 3306
+  security_rule {
+    name                       = "Allow_MySQL"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
--- infra/kstp/app/201-subnet/main.tf
+++ infra/kstp/app/201-subnet/main.tf
@@ -1,11 +1,24 @@
-data "azurerm_virtual_network" "kstp_network" {
-  name                = var.network
+##################################################################################
+# DATA SOURCES
+##################################################################################
+data "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
   resource_group_name = var.res_group
 }
 
+##################################################################################
+# CONFIGURATION
+##################################################################################
+# Create db subnet within virtual network
 resource "azurerm_subnet" "app_subnet" {
   name                 = "app-subnet"
   resource_group_name  = var.res_group
-  virtual_network_name = data.azurerm_virtual_network.kstp_network.name
+  virtual_network_name = var.network
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create security group associations
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  network_security_group_id = data.azurerm_network_security_group.app_nsg.id
+  subnet_id                 = azurerm_subnet.app_subnet.id
 }
